014c68c660337a6cf72b1ebba7c2327d4ab3d1c1
anujramola <omiramola100@gmail.com>
2024-10-25 13:36:01 +0530

    Updated app.py

--- Docker_Pipeline/app.py
+++ Docker_Pipeline/app.py
@@ -9,22 +9,22 @@ task_id_counter = 1
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global task_id_counter
-    response_text = ""
 
     if request.method == 'POST':
+        # Add a new task
         if 'add_task' in request.form:
             task_content = request.form.get('task_content')
             if task_content:
                 tasks[task_id_counter] = task_content
-                task_id_counter += 1
+                task_id_counter += 1  # Increment the ID counter
 
+        # Delete a task
         elif 'delete_task' in request.form:
             task_id_to_delete = int(request.form.get('task_id_to_delete'))
-            tasks.pop(task_id_to_delete, None)
+            tasks.pop(task_id_to_delete, None)  # Remove the task if it exists
 
-        
     return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
-    # app.run(port=5000,debug=True)
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    # Run the app on all interfaces and port 5000
+    app.run(host='0.0.0.0', port=5000, debug=True)
